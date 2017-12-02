ee1e4d1c824930052bc8b5859f74c3740be7ebb4
Alessandro Capra <alessandro.cpr@gmail.com>
2017-12-02 11:45:19 +0100

    Added Bower import

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/', index);
 app.use('/users', users);
 
 // create link to bower components, to load them in the pages we need
-
+app.use(express.static(__dirname + '/bower_components'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
