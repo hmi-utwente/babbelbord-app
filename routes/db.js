5f88b6157c8b75ec1f91e2239ac640e185950933
Alessandro Capra <alessandro.cpr@gmail.com>
2017-12-03 12:25:56 +0100

    Added database dependencies and created new route for it

---
+++ routes/db.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var pg = require('pg');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Babbelbord' });
+});
+
+router.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
+
+module.exports = router;
