0b285ffe5804af03bf316eed903f81f3c3cf0419
Alessandro Capra <alessandro.cpr@gmail.com>
2017-12-08 15:53:42 +0100

    Changed configuration to fix Heroku deployment

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  port: 8081,
+  port: process.env.PORT || 8081,
   db: {
     // change this with process.env.DATABASE_URL in the future
-    databaseURL: 'postgres://cyhqwpgxezzsiy:f67e2de03bb5fd2ea49b5fcef56898083d882ae9c88672f5188b026c0da1adb9@ec2-184-73-189-190.compute-1.amazonaws.com:5432/d3f8dhq054cbbf',
+    databaseURL: process.env.DATABASE_URL || 'postgres://cyhqwpgxezzsiy:f67e2de03bb5fd2ea49b5fcef56898083d882ae9c88672f5188b026c0da1adb9@ec2-184-73-189-190.compute-1.amazonaws.com:5432/d3f8dhq054cbbf',
     dialect: 'postgres',
     dialectOptions: {
       ssl: true
