36c2c8141e6bdbebc94a43c10bb5c9b0c4819adc
Alessandro Capra <alessandro.cpr@gmail.com>
2017-12-08 11:33:04 +0100

    Fixed readdirSync call in sequelize

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -7,22 +7,23 @@ const Sequelize = require('sequelize')
 
 const db = {}
 
-sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize('postgres://cyhqwpgxezzsiy:f67e2de03bb5fd2ea49b5fcef56898083d882ae9c88672f5188b026c0da1adb9@ec2-184-73-189-190.compute-1.amazonaws.com:5432/d3f8dhq054cbbf', {
   dialect:  'postgres',
   protocol: 'postgres',
-  port:     match[4],
-  host:     match[3],
+  port:     5432,
+  host:     'ec2-184-73-189-190.compute-1.amazonaws.com',
   logging:  true //false
 })
 
 fs
-  .readSync(__dirname)
+  .readdirSync(__dirname)
   .filter((file) =>
     file !== 'index.js'
   )
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname,file))
-    db[model.name] = model
+    var model = sequelize.import(path.join(__dirname,file))
+    console.log(model);
+    //db[model.name] = model
 })
 
 db.sequelize = sequelize
