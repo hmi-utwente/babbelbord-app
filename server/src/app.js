b8b2bec0fabe135196ab4c41ca887001246bdd22
Alessandro Capra <alessandro.cpr@gmail.com>
2017-12-11 09:56:27 +0100

    Trying to serve dist folder in Heroku

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path')
 const morgan = require('morgan')
 
 const {sequelize} = require('./models')
@@ -10,6 +11,8 @@ const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
+
+app.use(express.static(path.join(__dirname, 'dist')))
 
 require('./routes')(app)
 
