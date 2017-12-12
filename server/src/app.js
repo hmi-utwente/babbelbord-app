cf038fefc296e5296ee6ecf5c372690653e99690
Alessandro Capra <alessandro.cpr@gmail.com>
2017-12-12 16:45:52 +0100

    Fixed linting errors on server

--- server/src/app.js
+++ server/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
-const serveStatic = require('serve-static');
+const serveStatic = require('serve-static')
 const path = require('path')
 
 const {sequelize} = require('./models')
@@ -12,7 +12,7 @@ const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
-app.use(serveStatic(path.join(__dirname,'dist')));
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 require('./routes')(app)
 
