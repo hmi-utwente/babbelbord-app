0e907693db75ca82d494d0ff0d9c25bbe749b4e3
Alessandro Capra <alessandro.cpr@gmail.com>
2018-01-06 16:29:33 +0100

    Made the toolbar dynamic with event bus

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <v-toolbar color="amber" app>
-    <v-toolbar-title>Babbelbord</v-toolbar-title>
+    <v-btn v-if="backArrow" @click="goBack" icon class="hidden-xs-only">
+      <v-icon>arrow_back</v-icon>
+    </v-btn>
+    <v-toolbar-title>{{ title }}</v-toolbar-title>
     <v-spacer></v-spacer>
     <v-btn icon to="/">
       <v-icon>home</v-icon>
@@ -12,7 +15,25 @@
 </template>
 
 <script>
-
+  export default {
+    data () {
+      return {
+        title: '',
+        backArrow: false,
+      }
+    },
+    methods: {
+      goBack(){
+        this.$router.go(-1)
+      }
+    },
+    created(){
+      Event.$on('toolbar-data', (title, backArrow) => {
+        this.title = title;
+        this.backArrow = backArrow
+      })
+    }
+  }
 </script>
 
 <style scoped>
