3f3230e85db6b195935bbb87f289b5b1869a71df
Alessandro Capra <alessandro.cpr@gmail.com>
2018-01-12 20:02:24 +0100

    Removed url from constructor of Socket on client side

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -16,7 +16,7 @@
   import Question from './Question.vue'
   import Instruction from './Instruction.vue'
 
-  var socket = io('https://localhost:8081')
+  var socket = io()
 
   export default {
     components: { Question, Instruction },
